8476f7e91fa68b887ce058755c6fd61551f2b2fc
uracle <sodud@uracle.co.kr>
2024-01-03 21:48:43 +0900

    viewController to editViewController 메세지 전달

--- Navigation/EditViewController.swift
+++ Navigation/EditViewController.swift
@@ -7,16 +7,32 @@
 
 import UIKit
 
+protocol EditDelegate {
+    func didMessageEditDone(_ contoroller: EditViewController, message: String)
+}
+
 class EditViewController: UIViewController {
     var textWayValue : String = ""
+    var textMessage : String = ""
+    var delegate : EditDelegate?
+    
+    @IBOutlet var txtMessage: UITextField!
+    @IBOutlet var swisOn: UISwitch!
     @IBOutlet var lblWay: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         lblWay.text = textWayValue
+        txtMessage.text = textMessage
     }
     
     @IBAction func btnDone(_ sender: UIButton) {
+        //데이터를 메인화면으로 전달
+        if delegate != nil {
+            delegate?.didMessageEditDone(self, message: txtMessage.text!)
+        }
+        
+        _ = navigationController?.popViewController(animated: true)
     }
     
     /*
--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -7,20 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, EditDelegate {
+    
+    let imgOn = UIImage(named: "lamp_on.png")
+    let imgOff = UIImage(named: "lamp_off.png")
+    
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var txtMessage: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imgView.image = imgOn
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let editViewController = segue.destination as! EditViewController
         if segue.identifier == "editButton" {
             //버튼을 클릭한 경우
+            editViewController.textWayValue = "segue : use Button"
         }else if segue.identifier == "editBarButton"{
             //바 버튼을 클릭한 경우
+            editViewController.textWayValue = "segue : use Bar Button"
         }
+        editViewController.textMessage = txtMessage.text!
+        editViewController.delegate = self
+    }
+    
+    //EditViewController에서 함수를 호출하며 message를 전달
+    func didMessageEditDone(_ contoroller: EditViewController, message: String) {
+        txtMessage.text = message
     }
 
 }
